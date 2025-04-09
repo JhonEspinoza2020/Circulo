9f361d13fd6144d6f3e62b6ce968fadd65671fae
JhonEspinoza2020 <71272388@continental.edu.pe>
2025-04-09 11:20:09 -0500

    Se implemento la circunferencia

--- circulo.py
+++ circulo.py
@@ -1,2 +1,12 @@
 class Circulo:
-    pass
+    def __init__(self, radio):
+        self.PI = 3.1416
+        self.radio = radio
+
+    def circunferencia(self):
+        return 2 * self.PI * self.radio
+
+
+if __name__ == "__main__":
+    instancia_circulo = Circulo(10)
+    print(f"La circunferencia es: {instancia_circulo.circunferencia()}")
